14f4bc8badf789d8ae72805159ee35a77fbc004c
Maslovars <maslovars@gmail.com>
2024-05-04 17:30:56 +0300

    feat: add eslint and prettier acccording to RSS-ECOMM-1_10, RSS-ECOMM-1_11

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,17 +2,35 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
+    'airbnb',
+    'airbnb-typescript',
+    'airbnb/hooks',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  plugins: ['react-refresh', 'prettier'],
   rules: {
+    'react/jsx-uses-react': 0,
+    'react/react-in-jsx-scope': 0,
+    'linebreak-style': 0,
+    '@typescript-eslint/comma-dangle': 0,
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
   },
-}
+};
