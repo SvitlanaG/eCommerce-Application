7232288caad1a067490db672336f8786232c33b0
Svitlana <svitlana.grytsai@gmail.com>
2024-05-12 23:56:11 +0200

    fix: adjust 404 page layout, change login icon, add gh accounts and rs School link to footer

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,25 +1,33 @@
+import { FaSignInAlt } from 'react-icons/fa';
 import styles from './Header.module.scss';
+import btnStyles from '../../styles/components/buttons.module.scss';
+import appStyles from '../../App.module.scss';
 
 const Header = () => {
   return (
     <header className={styles.header}>
-      <div className={styles.logo}>
-        <img src="/logo.png" alt="Logo" />
-      </div>
-      <div className="flex flex-fw-wrap flex-fd-column">
-        <h1 className={styles['product-name']}>Book Lounge Online</h1>
-        <p
-          className={`${styles['product-name-slogan']} text-subtitle text-subtitle-large`}
-        >
-          Between Codes and Coffee, Find Time for Books
-        </p>
-      </div>
+      <div className={`${appStyles.container} flex flex-jc-space-between`}>
+        <div className={styles.logo}>
+          <img src="/logo.png" alt="Logo" />
+        </div>
+        <div className="flex flex-fw-wrap flex-fd-column">
+          <h1 className={styles['product-name']}>Book Lounge Online</h1>
+          <p
+            className={`${styles['product-name-slogan']} text-subtitle text-subtitle-large`}
+          >
+            Between Codes and Coffee, Find Time for Books
+          </p>
+        </div>
 
-      <div className={styles.login}>
-        <a href="https://github.com/SvitlanaG/eCommerce-Application">
-          <img src="/login.svg" alt="Login" />
-        </a>
-        <p className="text-button text-button-small">Login</p>
+        <div className={styles.login}>
+          <a
+            className={`${btnStyles['button-icon']} ${btnStyles['button-icon-secondary']}`}
+            href="https://github.com/SvitlanaG/eCommerce-Application"
+          >
+            <FaSignInAlt />
+          </a>
+          <p className="text-button text-button-small">Login</p>
+        </div>
       </div>
     </header>
   );
--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -1,8 +1,26 @@
+import book from '../assets/img/book.png';
+import btnStyles from '../styles/components/buttons.module.scss';
+import styles from './ErrorPage.module.scss';
+
 const ErrorPage = () => {
   return (
-    <div>
-      <h1>404 Not Found</h1>
-      <p>Sorry, the page you are looking for does not exist.</p>
+    <div className={styles.wrapper}>
+      <div className={styles.image}>
+        <img src={book} alt="orange book" />
+      </div>
+      <div className={styles.content}>
+        <h2>404 Page Not Found</h2>
+        <p className="text-body text-body-large">
+          Sorry, the page you are looking for does not exist. Return to our home
+          page to continue your journey.
+        </p>
+        <a
+          className={`${btnStyles['button-large']} ${btnStyles['button-primary']}`}
+          href="https://github.com/SvitlanaG/eCommerce-Application"
+        >
+          go home
+        </a>
+      </div>
     </div>
   );
 };
