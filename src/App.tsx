a9ba954c3054d6204b997cecccf565abd7d91e87
Maslovars <maslovars@gmail.com>
2024-05-12 05:22:07 +0300

    feat: add add layout with header and footer, update global styles, add mixins

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,29 @@
-import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MainPage from './pages/MainPage';
 import LoginPage from './pages/LoginPage';
 import RegistrationPage from './pages/RegistrationPage';
 import ErrorPage from './pages/ErrorPage';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import styles from './App.module.scss';
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="" element={<MainPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegistrationPage />} />
-        <Route path="*" element={<ErrorPage />} />
-      </Routes>
+      <div className={styles.app}>
+        <Header />
+        <main className={styles.main}>
+          <div className={styles.container}>
+            <Routes>
+              <Route path="" element={<MainPage />} />
+              <Route path="/login" element={<LoginPage />} />
+              <Route path="/register" element={<RegistrationPage />} />
+              <Route path="*" element={<ErrorPage />} />
+            </Routes>
+          </div>
+        </main>
+        <Footer />
+      </div>
     </BrowserRouter>
   );
 }
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,12 +1,7 @@
-import Footer from '../components/Footer/Footer';
-import Header from '../components/Header/Header';
-
 const MainPage = () => {
   return (
     <div>
-      <Header />
       <h2>Hello from main</h2>
-      <Footer />
     </div>
   );
 };
