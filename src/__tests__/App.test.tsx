3895c2c4fc003372fd1116c56a52dcd3b6ccdda0
Svitlana <svitlana.grytsai@gmail.com>
2024-05-21 21:27:59 +0200

    feat: add unit tests for catalog page and App

---
+++ src/__tests__/App.test.tsx
@@ -0,0 +1,16 @@
+import { render } from '@testing-library/react';
+import { describe, it } from 'vitest';
+import '@testing-library/jest-dom';
+import { Provider } from 'react-redux';
+import { store } from '../store/store';
+import App from '../App';
+
+describe('App component', () => {
+  it('renders the application correctly', () => {
+    render(
+      <Provider store={store}>
+        <App />
+      </Provider>,
+    );
+  });
+});
