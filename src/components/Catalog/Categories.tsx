679eb10b4447f1e7c5430f6f549a8ab589375c93
arastepa <stepanyan2298@gmail.com>
2024-05-31 22:42:36 +0400

    fix: move categories to custom hook

--- src/components/Catalog/Categories.tsx
+++ src/components/Catalog/Categories.tsx
@@ -1,18 +1,14 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import styles from '@/pages/CatalogPage/CatalogPage.module.scss';
 import getBooks from '@/services/getBooks';
 import Up from '@/assets/icons/up.svg';
 import Down from '@/assets/icons/down.svg';
-import { Category } from '@/types/categories';
-import getCategories from '@/services/catalog';
 import { PropsCategories } from '@/types/Props';
+import useFetch from '@/hooks/useFetch';
 
 const Categories = ({ onSetBooks }: PropsCategories) => {
-  const [categories, setCategories] = useState<Category[]>([]);
+  const categories = useFetch();
   const [isListVisible, setIsListVisible] = useState(false);
-  useEffect(() => {
-    getCategories().then((items) => setCategories(items));
-  }, []);
   return (
     <div className={styles.categories}>
       <span
