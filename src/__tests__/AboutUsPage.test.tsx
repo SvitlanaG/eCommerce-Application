15d850c07d7db30f18383aba03cc55207fdd5db2
Svitlana <svitlana.grytsai@gmail.com>
2024-06-18 13:28:23 +0200

    feat: add unit tests to Basket page

--- src/__tests__/AboutUsPage.test.tsx
+++ src/__tests__/AboutUsPage.test.tsx
@@ -10,7 +10,7 @@ const mockTeamMembers = [
     location: 'Yerevan',
     bio: 'Ara is a skilled developer capable of creating client-side websites. He is always eager to learn new technologies and languages. His technical stack includes C, C++, Docker-Compose, and JavaScript.He has completed the Common Core program at 42 international coding school.',
     contributions:
-      'Comprehensive README, CommerceTools Project and API Client Setup, Main page, 404 page, SUBMIT on Registration, Catalog Product page, Intergration of Login and Registration pages with CommerceTools',
+      'Comprehensive README, CommerceTools Project and API Client Setup, Main Page, 404 Page, Submit on Registration, Catalog Product Page, Intergration of Login and Registration Pages with CommerceTools, Basket Page',
     collaboration:
       'Ara is a driven and initiative-taking team member who consistently aims to achieve the highest results. He effectively collaborated with the team. His purposefulness ensured that all project milestones were met on time. As a software developer, he played a crucial role in the successful completion of our project.',
     photo: '@/assets/fotos/ara.png',
@@ -22,7 +22,7 @@ const mockTeamMembers = [
     location: 'Berlin',
     bio: 'Svitlana is an expat living in Germany who graduated HTW Berlin with a degree in Business Informatics. Currently working as a developer, she has a passion for creating user interfaces and specializes in making online forms for government institutions. Dedicated to continuous learning, she participates in RS School courses to master new technologies and apply them in her work.',
     contributions:
-      'Repository Setup, Task Board Setup, Unit Test Coverage, Deploy the App, Basic styles, Footer and Header, Login Page, Registration page, User Profile page, Intergration of User Profile with CommerceTools, About Us Page, Main page, 404 page',
+      'Repository Setup, Task Board Setup, Unit Test Coverage, Deploy the App, Basic styles, Footer and Header, Login Page, Registration Page, User Profile Page, Intergration of User Profile with CommerceTools, About Us Page, Main Page, 404 Page, Basket Page',
     collaboration:
       'Svitlana effectively organized collaboration within the team, utilizing scrum methodologies and various online tools. She used Jira for task distribution, managed communication via a Discord server, and facilitated online meetings through Zoom. Her dedication to excellence and strong teamwork skills were pivotal in achieving our project goals.',
     photo: '@/assets/fotos/svitlana.jpg',
@@ -34,7 +34,7 @@ const mockTeamMembers = [
     location: 'Minsk',
     bio: 'Arseny discovered RS-School through acquaintances who had already graduated. With previous front-end learning experience but having taken a long break, he enrolled in Stage 0 this summer to refresh his basics. Simultaneously, he signed up for an RS-School React course. Successfully completing both courses with certificates, he advanced to Stages 1-2 to further enhance his skills in JavaScript and layout development.',
     contributions:
-      'Development Environment Configuration, Development Scripts, Deploy the App, Login Page, Registration Page, Detailed Product Page,Intergration of Product Page with CommerceTools.',
+      'Development Environment Configuration, Development Scripts, Deploy the App, Login Page, Registration Page, Detailed Product Page,Intergration of Product Page with CommerceTools, Basket Page.',
     collaboration:
       'Arseny had an idea to use React in the team project, leveraging his familiarity with the technology. He actively supported his teammates by answering their questions and providing guidance on React, contributing significantly to the projects success.',
     photo: '@/assets/fotos/arseny.png',
